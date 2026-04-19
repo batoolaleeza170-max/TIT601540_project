313b21234ae97ff489f624822656f3907fb8a914
Esha Shaukat <eshashaukat0752@gmail.com>
2026-04-20 01:59:22 +0500

    Update home_screen.dart

--- lab_5/lib/screens/home_screen.dart
+++ lab_5/lib/screens/home_screen.dart
@@ -2,19 +2,21 @@ import 'package:flutter/material.dart';
 import '../widgets/status_image.dart';
 import '../widgets/control_icons.dart';
 import '../widgets/command_form.dart';
+import '../widgets/animations.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("NOVA Wheelchair Control Lab"),
-        centerTitle: true,
+        title: Text("NOVA Smart Wheelchair"),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
             StatusImage(),
+            AnimatedMovement(),
             ControlIcons(),
+            AnimatedControls(),
             CommandForm(),
           ],
         ),
