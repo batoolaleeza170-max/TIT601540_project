a9dbfff672364fc6f7a45ad99400ae4fd4267689
Nabeel ALi <saithnabeel35@gmail.com>
2026-05-06 23:35:31 +0500

    Update home_screen.dart

--- lab_5/lib/screens/home_screen.dart
+++ lab_5/lib/screens/home_screen.dart
@@ -1,25 +1,52 @@
 import 'package:flutter/material.dart';
-import '../widgets/status_image.dart';
-import '../widgets/control_icons.dart';
-import '../widgets/command_form.dart';
-import '../widgets/animations.dart';
-class HomeScreen extends StatelessWidget {
+import '../services/api_service.dart';
+import '../models/user_model.dart';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+class _HomeScreenState extends State<HomeScreen> {
+  List<UserModel> users = [];
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+  void fetchUsers() async {
+    try {
+      final data = await ApiService().getUsers();
+      setState(() {
+        users = data;
+        isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("NOVA Smart Wheelchair"),
+        title: const Text("NOVA Dashboard"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            StatusImage(),
-            AnimatedMovement(),
-            ControlIcons(),
-            AnimatedControls(),
-            CommandForm(),
-          ],
-        ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: const EdgeInsets.all(10),
+            child: ListTile(
+              leading: const Icon(Icons.person, color: Colors.blue),
+              title: Text(users[index].name),
+              subtitle: Text(users[index].email),
+            ),
+          );
+        },
       ),
     );
   }
